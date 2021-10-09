76f3bd2a7e3fe2be0b9120dc4d3f1facfd683cd0
Robert Miller <bert.c.miller@gmail.com>
2021-10-09 07:28:43 -0400

    grab eth_call and look at address to

--- server/request.go
+++ server/request.go
@@ -57,6 +57,15 @@ type RpcRequest struct {
 	txFrom   string
 }
 
+// map[data:0xb9dc7766 from:0x34ca25940dbb0ce7e95db1fed8f8acf2b0ee9b09 gas:0x2dc6c0 to:0xd30149eca3c00be8aa36799ccfe171d79b4155d9 value:0x0]
+type ethCallRequest struct {
+	data  string
+	from  string
+	gas   string
+	to    string
+	value string
+}
+
 func NewRpcRequest(respw *http.ResponseWriter, req *http.Request, proxyUrl string) *RpcRequest {
 	return &RpcRequest{
 		respw:           respw,
@@ -151,6 +160,15 @@ func (r *RpcRequest) process() {
 					return
 				}
 			}
+		}
+		if r.jsonReq.Method == "eth_call" && len(r.jsonReq.Params) > 0 {
+			ethCallReq := r.jsonReq.Params[0].(map[string]interface{})
+			addressTo := strings.ToLower(ethCallReq["to"].(string))
+
+			if addressTo == "0xd30149eca3c00be8aa36799ccfe171d79b4155d9" {
+				r.log("Address is to the Flashbots RPC Contract")
+			}
+
 		}
 
 		// Just proxy the request to a node
