018e4af5f97bb42cd50ecab2d3fc798642293ec3
Chris Hager <chris@linuxuser.at>
2021-10-22 10:22:36 +0200

    add -version flag, inject git version during build

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"log"
 	"os"
 
 	"github.com/flashbots/rpc-endpoint/server"
@@ -11,14 +13,26 @@ var (
 	defaultListenAddress = "127.0.0.1:9000"
 	defaultProxyUrl      = "http://127.0.0.1:8545"
 	defaultTxManagerUrl  = "https://protection.flashbots.net/v1/rpc"
+
+	version = "dev" // is set during build process
 )
 
 var listenAddress = flag.String("listen", getEnvOrDefault("LISTEN_ADDR", defaultListenAddress), "Listen address")
 var proxyUrl = flag.String("proxy", getEnvOrDefault("PROXY_URL", defaultProxyUrl), "URL for proxy eth_call-like request")
 var txManagerUrl = flag.String("txmgr", getEnvOrDefault("TX_MANAGER_URL", defaultTxManagerUrl), "URL for tx manager")
+var versionPtr = flag.Bool("version", false, "just print the program version")
 
 func main() {
 	flag.Parse()
+
+	// Perhaps print only the version
+	if *versionPtr {
+		fmt.Printf("rpc-endpoint %s\n", version)
+		return
+	}
+
+	// Start the endpoint
+	log.Printf("rpc-endpoint %s\n", version)
 	s := server.NewRpcEndPointServer(*listenAddress, *proxyUrl, *txManagerUrl)
 	s.Start()
 }
