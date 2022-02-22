43035104f722eed970962a8cfa067e1a71005f26
Bhakiyaraj Kalimuthu <bhakiya.kalimuthu@gmail.com>
2022-02-22 22:45:45 +0100

    add docker compose

---
+++ server/request_record.go
@@ -0,0 +1,45 @@
+package server
+
+import (
+	"github.com/flashbots/rpc-endpoint/database"
+	"github.com/flashbots/rpc-endpoint/types"
+	"net/http"
+	"sync"
+)
+
+type requestRecord struct {
+	requestEntry        *database.RequestEntry
+	ethSendRawTxEntries []*database.EthSendRawTxEntry
+	mutex               *sync.Mutex
+}
+
+func NewRequestRecord() *requestRecord {
+	return &requestRecord{
+		requestEntry:        &database.RequestEntry{},
+		ethSendRawTxEntries: make([]*database.EthSendRawTxEntry, 0),
+		mutex:               &sync.Mutex{},
+	}
+}
+
+func (r *requestRecord) UpdateEthSendRawTxEntries(rpcReq *types.JsonRpcRequest) *database.EthSendRawTxEntry {
+	var ethSendRawTxEntry *database.EthSendRawTxEntry
+	if rpcReq.Method == "eth_sendRawTransaction" {
+		ethSendRawTxEntry = &database.EthSendRawTxEntry{}
+		r.mutex.Lock()
+		defer r.mutex.Unlock()
+		r.ethSendRawTxEntries = append(r.ethSendRawTxEntries, ethSendRawTxEntry)
+	}
+	return ethSendRawTxEntry
+}
+
+func (r *requestRecord) UpdateRequestEntry(req *http.Request, reqStatus int, error string) {
+	// TODO:Error should be converted to enum
+	r.requestEntry.HttpMethod = req.Method
+	r.requestEntry.IpHash = GetIPHash(req)
+	r.requestEntry.Error = error
+	r.requestEntry.HttpUrl = req.URL.Path
+	r.requestEntry.HttpQueryParam = req.URL.RawQuery
+	r.requestEntry.HttpResponseStatus = reqStatus
+	r.requestEntry.Origin = req.Header.Get("Origin")
+	r.requestEntry.Host = req.Header.Get("Host")
+}
