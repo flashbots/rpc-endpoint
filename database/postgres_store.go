ec57b3b0d997dc07c2a729d09da62547a582f2e0
Bhakiyaraj Kalimuthu <bhakiya.kalimuthu@gmail.com>
2022-03-07 13:06:50 +0100

    remove inserted_at

--- database/postgres_store.go
+++ database/postgres_store.go
@@ -27,7 +27,6 @@ func (d *postgresStore) Close() {
 }
 
 func (d *postgresStore) SaveRequestEntry(entry RequestEntry) error {
-	entry.InsertedAt = time.Now()
 	query := `INSERT INTO rpc_endpoint_requests 
 	(id, received_at, inserted_at, request_duration_ms, is_batch_request, num_request_in_batch, http_method, http_url, http_query_param, http_response_status, ip_hash, origin, host, error) VALUES (:id, :received_at, :inserted_at, :request_duration_ms, :is_batch_request, :num_request_in_batch, :http_method, :http_url, :http_query_param, :http_response_status, :ip_hash, :origin, :host, :error)`
 	ctx, cancel := context.WithTimeout(context.Background(), connTimeOut)
