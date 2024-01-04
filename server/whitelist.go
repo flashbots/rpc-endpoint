dd9b1acf46fe1971a557950b8bd96ab106d43ef5
remollemo <43035129+remollemo@users.noreply.github.com>
2024-01-04 14:54:01 +0200

    Whitelist StarkWare testnet Sharp verifiers (#132)

--- server/whitelist.go
+++ server/whitelist.go
@@ -22,7 +22,9 @@ func isOnFunctionWhitelist(data string) bool {
 
 var allowedLargeTxTargets = map[string]bool{
 	"0xff1f2b4adb9df6fc8eafecdcbf96a2b351680455": true, // Aztec rollup contract
-	"0x47312450B3Ac8b5b8e247a6bB6d523e7605bDb60": true, // StarkWare SHARP Verifier
+	"0x47312450B3Ac8b5b8e247a6bB6d523e7605bDb60": true, // StarkWare SHARP Verifier (Mainnet)
+	"0x8f97970aC5a9aa8D130d35146F5b59c4aef57963": true, // StarkWare SHARP Verifier (Goerli)
+	"0x07ec0D28e50322Eb0C159B9090ecF3aeA8346DFe": true, // StarkWare SHARP Verifier (Sepolia)
 }
 
 func init() {
